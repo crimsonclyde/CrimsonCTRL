6738246d0a36f8fd7c66fcc3b013e2c92107d7da
CrimsonClyde <clyde@darkpack.net>
2015-02-02 19:10:23 +0100

    Add: More Log.d for debugging Update: Error handling Update: Code cleaning

--- app/src/main/java/net/darkpack/crimsonctrl/MainActivity.java
+++ app/src/main/java/net/darkpack/crimsonctrl/MainActivity.java
@@ -18,6 +18,7 @@ package net.darkpack.crimsonctrl;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -39,8 +40,7 @@ import java.io.PrintWriter;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 
-
-
+import static net.darkpack.crimsonctrl.SettingsConnector.getEditor;
 
 
 public class MainActivity extends Activity {
@@ -62,53 +62,12 @@ public class MainActivity extends Activity {
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         if (isNetworkAvailable()) {
 
-
             mProgressBar.setVisibility(View.VISIBLE);
             loadCrimsonCoreData();
-
         } else {
             Toast.makeText(this, "Network is unavailable!", Toast.LENGTH_LONG).show();
         }
-
-
-
     }
-
-    private boolean isNetworkAvailable() {
-        ConnectivityManager manager = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
-
-        boolean isAvailable = false;
-        if (networkInfo != null && networkInfo.isConnected()) {
-            isAvailable = true;
-        }
-        return isAvailable;
-    }
-
-
-    public void scButtonListener() {
-
-        scButton = (Button) findViewById(R.id.scButtonSelector);
-
-        scButton.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                // Get CoreID + AccessToken from shared preferences
-                String coreid = SettingsConnector.readString(MainActivity.this, SettingsConnector.COREID, null);
-                String accesstoken = SettingsConnector.readString(MainActivity.this, SettingsConnector.ACCESSTOKEN, null);
-                // Execute
-                new SclCtrl().execute("https://api.spark.io/v1/devices/"+coreid+"/events/?access_token="+accesstoken);
-
-                return false;
-            }
-        });
-
-    }
-
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -168,49 +127,100 @@ public class MainActivity extends Activity {
 
     }
 
+    /*
+    * IsNetworkAvailable
+    *
+    * Check if Network is up and running
+    */
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
 
 
     /*
-    * Requesting data from shared preferences
+    * Create OnClickListener
+    *
+    * Create listener for the refresh function
     */
-    private void loadCrimsonCoreData() {
-        // Get CoreID + AccessToken from shared preferences
-        String coreid = SettingsConnector.readString(this, SettingsConnector.COREID, null);
-        String accesstoken = SettingsConnector.readString(this, SettingsConnector.ACCESSTOKEN, null);
-        Log.d(TAG, "CoreID:        " + coreid);
-        Log.d(TAG, "AccessToken:   " + accesstoken);
-        // Get values from spark-cloud
-        new HttpsClient().execute("https://api.spark.io/v1/devices/"+coreid+"/events/?access_token="+accesstoken);
+    public void scButtonListener() {
+
+        scButton = (Button) findViewById(R.id.scButtonSelector);
+
+        scButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                // Get CoreID + AccessToken from shared preferences
+                String coreid = SettingsConnector.readString(MainActivity.this, SettingsConnector.COREID, null);
+                String accesstoken = SettingsConnector.readString(MainActivity.this, SettingsConnector.ACCESSTOKEN, null);
+                // Execute
+                new SclCtrl().execute("https://api.spark.io/v1/devices/"+coreid+"/events/?access_token="+accesstoken);
+
+                return false;
+            }
+        });
 
     }
 
 
 
     /*
-    * Refresh values from the spark-cloud
+    * Initiate HttpClient
+    *
+    * Retrieve CoreID & AccessToken from SharedPreferences
+    * Execute HttpClient
+    */
+    private void loadCrimsonCoreData() {
+        // Get CoreID + AccessToken from shared preferences
+        String coreid = SettingsConnector.readString(this, SettingsConnector.COREID, null);
+        String accesstoken = SettingsConnector.readString(this, SettingsConnector.ACCESSTOKEN, null);
+        Log.d(TAG, "************    Retrieve data from Shared Preferences     ************");
+        Log.d(TAG, "CoreID:        " + coreid);
+        Log.d(TAG, "AccessToken:   " + accesstoken);
+
+        Log.d(TAG, "*********************    Execute HttpsClient     *********************");
+        new HttpsClient().execute("https://api.spark.io/v1/devices/"+coreid+"/events/?access_token="+accesstoken);
+    }
+
+
+    /*
+    * Restful Events from Spark-Cloud
+    *
+    * GET values stored in the cloud via httpsurlconnection
+    * and save 'em in Androids SharedPreferences
     */
     class HttpsClient extends AsyncTask<String, Void, String> {
+        public static final String PREFS_NAME = "TEMP_SETTINGS";
+        public static final String mPHOTO = "PHOTO";
+        public static final String mTEMP = "TEMP";
+        public static final String mSCL = "SCL";
         private Exception exception;
+
+
         public String doInBackground(String... urls) {
 
             try {
-                Log.d(TAG, "*******************    Open Connection    *****************************");
+                Log.d(TAG, "**********************************************************************");
+                Log.d(TAG, "******************    HttpsUrlConnection    **************************");
                 URL url = new URL(urls[0]);
                 Log.d(TAG, "Received URL:  " + url);
-
                 HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
                 Log.d(TAG, "Con Status: " + con);
-
                 InputStream in = con.getInputStream();
                 Log.d(TAG, "GetInputStream:  " + in);
 
                 Log.d(TAG, "*******************    String Builder     *****************************");
                 String line = null;
-
                 BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
                 Data data = new Data();
-
                 while ((line = br.readLine()) != null) {
                     if (line.contains("event")) {
                         //do nothing since the event tag is of no interest
@@ -257,70 +267,84 @@ public class MainActivity extends Activity {
                     //check if we have all needed data
                     if (data.isReady()) {
                         //exit endless connection
-                        Log.d(TAG, "*******************    Data converted     *****************************");
+                        Log.d(TAG, "*********************    Data received    ****************************");
                         Log.d(TAG, "data:  " + data);
                         Log.d(TAG, "Datestamp test:  " + data.getDatestamp());
                         break;
                     }
                 }
 
+                // Now we create finalized containers for further usage
+                Log.d(TAG, "**********************************************************************");
+                Log.d(TAG, "*********************    Data received    ****************************");
+                Log.d(TAG, "gDate:   " + data.getDate());
+                Log.d(TAG, "gTime:   " + data.getTime());
+                Log.d(TAG, "gWifi:   " + data.getWifi().toString());
+                Log.d(TAG, "gTemp:   " + data.getTemp());
+                Log.d(TAG, "gPhoto:  " + data.getPhoto().toString());
+                Log.d(TAG, "gScl:    " + data.getScl().toString());
 
-                // Creation of finalized containers for UI usage
-                final String gTemp = data.getTemp() + "";               // Needs to be converted to a string
-                final String gPhoto = data.getPhoto() + "";             // Needs to be converted to a string
-                final int gScl = data.getScl();                         // Needs to be converted to a string
+
+
+                // Let´s put the results into the shared preferences
+                // TODO: Time+Date query if stored data <1Minute use data from shared preferences
+                Log.d(TAG, "***********    Store Data in Shared Preferences     ******************");
+                final SharedPreferences[] sharedValues = {MainActivity.this.getSharedPreferences(PREFS_NAME, MODE_PRIVATE)};
+                SharedPreferences.Editor editor = sharedValues[0].edit();
+                editor.clear();
+                editor.putString("PHOTO", data.getPhoto().toString());
+                editor.putString("TEMP", data.getTemp());
+                editor.putString("SCL", data.getScl().toString());
+                editor.apply();
 
 
                 // To access the findViewById we need this to runOnUiThread
-                runOnUiThread(new Runnable(){
+                runOnUiThread(new Runnable() {
                     public void run() {
                         Log.d(TAG, "*******************    Run UI Thread     *****************************");
-                        //Log.d(TAG, "gDate:   " + gDate);
-                        //Log.d(TAG, "gTime:   " + gTime);
-                        //Log.d(TAG, "gWifi:   " + gWifi);
-                        Log.d(TAG, "gTemp:   " + gTemp);
-                        Log.d(TAG, "gPhoto:  " + gPhoto);
-                        Log.d(TAG, "gScl:    " + gScl);
-
-                        // Assign and declare
-                        //final TextView updateDate  = (TextView) findViewById(R.id.dataDateTextView);
-                        //final TextView updateTime  = (TextView) findViewById(R.id.dataTimeTextView);
-                        //final TextView updateWifi  = (TextView) findViewById(R.id.dataWifiTextView);
-                        final TextView updateTemp  = (TextView) findViewById(R.id.textTemperature);
+                        // Assign & declare TextViews
+                        final TextView updateTemp = (TextView) findViewById(R.id.textTemperature);
                         final TextView updatePhoto = (TextView) findViewById(R.id.textResistance);
-                        final TextView updateScl   = (TextView) findViewById(R.id.textStoneCircle);
+                        final TextView updateScl = (TextView) findViewById(R.id.textStoneCircle);
 
 
+                        // Retrieve data from shared preferences
+                        Log.d(TAG, "************    Retrieve data from Shared Preferences     ************");
+                        sharedValues[0] = MainActivity.this.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+                        final String photo = sharedValues[0].getString("PHOTO", "");
+                        final String temp = sharedValues[0].getString("TEMP", "");
+                        final int scl = Integer.parseInt(sharedValues[0].getString("SCL", ""));
+                        Log.d(TAG, "photo var in shared pref: " + photo);
+                        Log.d(TAG, "temp var in shared pref: " + temp);
+                        Log.d(TAG, "scl var in shared pref: " + scl);
 
 
                         // Update the TextViews
-                        Log.d(TAG, "*******************    Update TextView       *****************************");
-                        //updateDate.setText(gDate);
-                        //updateTime.setText(gTime);
-                        //updateWifi.setText(gWifi);
-                        updateTemp.setText(gTemp);
-                        updatePhoto.setText(gPhoto);
-                        if (gScl >= 1) {
-                            updateScl.setText("Off");
-                        }
-                        if (gScl == 0) {
-                            updateScl.setText("On");
-                        }
+                        Log.d(TAG, "*****************    Update TextView       ***************************");
+                        if      (temp != null)  { updateTemp.setText(temp); }
+                        else                    { updateTemp.setText("ERROR"); }
+
+                        if      (photo != null) { updatePhoto.setText(photo); }
+                        else                    { updatePhoto.setText("ERROR"); }
+
+                        if      (scl == 1)  { updateScl.setText("Off"); }
+                        else if (scl == 0)  { updateScl.setText("On");  }
+                        else                { updateScl.setText("ERROR"); }
                     }
 
                 });
+
+                Log.d(TAG, "****************  Closing stream, exiting     ************************");
+
                 // Closing the stream
-                Log.d(TAG, "*******************  Stream closed, exiting     *****************************");
                 br.close();
                 mProgressBar.setVisibility(View.INVISIBLE);
+                Log.d(TAG, "**********************************************************************");
             } catch (Exception e) {
                 this.exception = e;
                 return null;
             }
             return null; }
-
-
-
     }
 
 
@@ -442,7 +466,11 @@ public class MainActivity extends Activity {
 
     }
 
-
+    /*
+    * Restful Function
+    *
+    * POST value to the function initialized by CrimsonCore
+    */
     class PostClient extends AsyncTask<String, Void, String> {
         public String doInBackground(String... IO) {
 
@@ -451,14 +479,17 @@ public class MainActivity extends Activity {
             URL url;
 
             try {
-                // Fetch CoreID + AccessToken from SharedPreferences and constuct url and param string
-                Log.d(TAG, "*******************   Fetch Results    *****************************");
+                // Fetch CoreID + AccessToken from SharedPreferences and construct url and param string
+                Log.d(TAG, "********************************************************************");
+                Log.d(TAG, "**********   Retrieve value from SharedPreferences   ***************");
                 String coreid = SettingsConnector.readString(MainActivity.this, SettingsConnector.COREID, null);
                 String accesstoken = SettingsConnector.readString(MainActivity.this, SettingsConnector.ACCESSTOKEN, null);
                 String ctrlpin = SettingsConnector.readString(MainActivity.this, SettingsConnector.CTRLPIN, null);
                 String ctrlvalue = SettingsConnector.readString(MainActivity.this, SettingsConnector.CTRLVALUE, null);
                 Log.d(TAG, "CoreID:       " + coreid);
                 Log.d(TAG, "AccessToken:  " + accesstoken);
+                Log.d(TAG, "CtrlPin:      " + ctrlpin);
+                Log.d(TAG, "CtrlValue:    " + ctrlvalue);
                 Log.d(TAG, "*******************   Building URL     *****************************");
                 url = new URL("https://api.spark.io/v1/devices/"+coreid+"/"+ctrlvalue+"/");
                 Log.d(TAG, "URL:          " + url);
@@ -477,14 +508,17 @@ public class MainActivity extends Activity {
                 con.setRequestMethod("POST");
                 con.setFixedLengthStreamingMode(param.getBytes().length);
                 con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                Log.d(TAG; "Con:       " + con);
 
-                // Send
+                Log.d(TAG, "********************   Send params   ****************************");
+                // Send specified parameters to the api (registered function must be set by CrimsonCore)
                 PrintWriter out = new PrintWriter(con.getOutputStream());
                 out.print(param);
                 out.close();
-
                 con.connect();
 
+                Log.d(TAG, "*****************   Buffered Reader  ***************************");
+                // Retrieve information if connection was successful or not
                 BufferedReader in = null;
                 if (con.getResponseCode() != 200) {
                     in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
@@ -500,11 +534,9 @@ public class MainActivity extends Activity {
                 e.printStackTrace();
                 return null;
             }
-            // Set null and we´e good to go
+            // Set null and we´re good to go
             return null;
         }
     }
-
-
 
 }
